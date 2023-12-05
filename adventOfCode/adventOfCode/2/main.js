22e973acf7eb5ed05485f00ed032b41c39d23d5a
TiaSous <tiago.rodrigues2@eduvaud.ch>
2023-12-05 22:43:35 +0100

    tentative day 2 but not finish

--- adventOfCode/adventOfCode/2/main.js
+++ adventOfCode/adventOfCode/2/main.js
@@ -1,28 +1,75 @@
-let total = 0
+let total = 0;
 
-document.getElementById('file').onchange = function(){
+document.getElementById('file').onchange = function () {
     var file = this.files[0];
     var reader = new FileReader();
-    reader.onload = function(progressEvent) {
+    reader.onload = function (progressEvent) {
         let lines = this.result.split(/\r\n|\n/);
 
-        for(let line = 0; line < lines.length - 1; line++)
-        {
+        for (let line = 0; line < lines.length; line++) {
             let number = lines[line].split('');
             let idGame;
-            for(let car = 0; car < number.length; car++)
-            {
-                if(number[car] >= '0' && number[car] <= '9' && car == 6)
-                {
+            let red = 0;
+            let blue = 0;
+            let green = 0;
+            
+            //get id
+            for (let car = 0; car < number.length; car++) {
+                if (number[car] >= '0' && number[car] <= '9' && car == 5) {
                     idGame = number[car];
-                    if(number[car+1] >= '0' && number[car+1] <= '9')
-                    {
-                        idGame += number[car+1];
+                    if (number[car + 1] >= '0' && number[car + 1] <= '9') {
+                        idGame += number[car + 1];
+                        if (number[car + 2] >= '0' && number[car + 2] <= '9') {
+                            idGame += number[car + 2];
+                        }
                     }
-                    console.log(idGame)
+                    //console.log(idGame)
+                }
+            }
+
+            for (let car = 0; car < number.length; car++) {
+                if (getNumberColor('r', number, car) != null)
+                {
+                    red += parseInt(getNumberColor('r', number, car))
+                }   
+                else if (getNumberColor('g', number, car) != null)
+                {
+                    green += parseInt(getNumberColor('g', number, car))
+                }
+                else if (getNumberColor('b', number, car) != null)
+                {
+                    blue += parseInt(getNumberColor('b', number, car))
+                }
+            }
+            //console.log(red, green, blue)
+            if(red <= 12){
+                console.log("a")
+                console.log(idGame)
+                console.log(red, green, blue)
+            }
+            if (red <= 12 && green <= 13 && blue <= 14)
+            {
+                // console.log("a")
+                // console.log(idGame)
+                total += parseInt(idGame)
+            }
+       
+        }
+        console.log(total);
+        
+    }
+    reader.readAsText(file);
+
+    function getNumberColor(color, number, car) {
+        let valeur = null
+        if (number[car] == color) {
+            if (number[car - 2] >= '0' && number[car - 2] <= '9') {
+                valeur = number[car - 2]
+                if (number[car - 3] >= '0' && number[car - 3] <= '9') {
+                    valeur = number[car - 3] + valeur
                 }
             }
         }
+        return valeur;
     }
-    reader.readAsText(file);
 }
